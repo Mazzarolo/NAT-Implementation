7a4db9cbc79a8a190520413bbc92236371f2b1d5
Joao <jdrmazzarolo@outlook.com>
2023-09-17 12:55:34 -0300

    gamer

--- Topo1/1-fwd.py
+++ Topo1/1-fwd.py
@@ -5,9 +5,11 @@ from scapy.all import *
 def example(pkt):
 	pkt.show()
 	if pkt.sniffed_on == 'r-eth1' and pkt[IP].dst == '10.2.2.1':
+		print("Host 1")
 		pkt[Ether].dst = None
 		sendp(pkt, iface='r-eth2')
 	elif pkt.sniffed_on == 'r-eth2' and pkt[IP].dst == '10.1.1.1':
+		print("Host 2")
 		pkt[Ether].dst = None
 		sendp(pkt, iface='r-eth1')
 	else:
