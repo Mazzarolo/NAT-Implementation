ef1b99c5939cff398b80a277c36e5c15236673e2
Luís Henrique Chesani <92533013+LChesani@users.noreply.github.com>
2023-09-26 13:47:21 +0000

    Update NAT.py

--- NAT.py
+++ NAT.py
@@ -1,35 +1,32 @@
 #!/usr/bin/env python
 
 from scapy.all import *
-from scapy.layers.inet import TCP, UDP, IP
+from scapy.layers.inet import IP
 from scapy.layers.l2 import Ether
-from Tabela import Tabela
+from Tabela import *
+
+import os
+
+os.system('ifconfig r-eth0 mtu 10000 up && ifconfig r-eth1 mtu 10000 up')
 
 table = Tabela()
 
-def whichProtocol(pkt):
-    if pkt.haslayer(TCP):
-        return TCP
-    elif pkt.haslayer(UDP):
-        return UDP
-    else:
-        return None
 
 def printInfo(pkt):
     print(f"IP src: {pkt[IP].src}")
     print(f"IP dst: {pkt[IP].dst}")
-    protocol = whichProtocol(pkt)
+    protocol = table.whichProtocol(pkt)
     print(protocol)
     if protocol != None:
         print(f"IP sport: {pkt[protocol].sport}")
         print(f"IP dport: {pkt[protocol].dport}")
     else:
-        print("Bosta cu merda mijo")
+        print("cachorro")
 
 
 def NAT(pkt):
-    routerIp = get_if_addr("r-eth1")
-    protocol = whichProtocol(pkt)
+    routerIp = get_if_addr(conf.iface)
+    protocol = table.whichProtocol(pkt)
     pkt[Ether].src = None
     pkt[Ether].dst = None
     pkt[IP].chksum = None
@@ -41,22 +38,20 @@ def NAT(pkt):
                 pkt[protocol].dport = reg.portPriv
                 pkt[IP].dst = reg.endPriv
                 pkt[IP].src = reg.endExt
-                #table.registros.remove(reg)
+                table.registros.remove(reg)
 
-        sendp(pkt, iface='r-eth0', verbose=False)
+        sendp(pkt, iface='r-eth0')
     else:
-        if pkt[IP].src[0] != '8' and pkt[IP].src != routerIp:
+        if pkt[IP].src != routerIp and pkt[IP].src[0] != '8':
             table.adicionar(pkt, pkt[protocol].sport, pkt[protocol].dport)
         pkt[IP].src = routerIp
 
-        sendp(pkt, iface='r-eth1', verbose=False)
-    #table.Print()
+        sendp(pkt, iface='r-eth1')
+    table.Print()
     #print('source = ', pkt[IP].src)
     #print('destiny = ', pkt[IP].dst)
     
 
 
-print(f"conf.iface={conf.iface}")
-print(f"get_if_addr(conf.iface={get_if_addr(conf.iface)}")
-print(f"get_if_addr(\"r-eth1\"={get_if_addr('r-eth1')}")
+
 sniff(iface=["r-eth0","r-eth1"], filter='ip',  prn=NAT)
